834cc4120a1a7706b45767323de2dcc27a542018
danny <danny.brogno@gmail.com>
2025-11-27 05:10:17 +0100

    addition of extra btn and functions

--- src/App.js
+++ src/App.js
@@ -9,18 +9,60 @@ const JobCounter = () => {
   //   console.log(jobCount); 
   // }
   
-  const [first, second] = useState(0);
+  const [currentNumber, updateNumber] = useState(0);
+  const [jobMessage, setJobMessage] = useState("No jobs available");
   
-  function handleAddJob() {
-    second(first + 1);
-    console.log(first + 1);
+  // Function to determine and set the message based on the count
+  function updateJobMessage(newCount) {
+    
+    let message;
+    
+    if (newCount === 0) {
+      message = `No jobs available`;
+    } else if (newCount > 0 && newCount < 5) {
+      message = `Few jobs available`;
+    } else if (newCount >= 5) {
+      message = `Many jobs available`;
+    } else if(newCount < 0) {
+      message = `You are below zero jobs`;
+    }
+    setJobMessage(message);
+    console.log(message);
+  };
+  
+  // -----------------------------------------
+  
+  function addJob() {
+    let newNumber = currentNumber + 1;
+    updateNumber(newNumber);
+    updateJobMessage(newNumber);
+    console.log(newNumber);
   }
-
-  return (
+  
+  function removeJob() {
+    let newNumber = currentNumber - 1;
+    updateNumber(newNumber);
+    updateJobMessage(newNumber);
+    console.log(newNumber);
+  }
+  
+  function reset() {
+    let newNumber = 0;
+    updateNumber(newNumber);
+    updateJobMessage(newNumber);
+    console.log(newNumber);
+  }
+  
+  // -----------------------------------------
+  
+  return(
     <div>
       <h1>Job Counter</h1>
-      <p>Current Jobs: {first}</p>
-      <button onClick={handleAddJob}>Add Job</button>
+      <p>Count: {currentNumber}</p> 
+      <p>Status: {jobMessage}</p>
+      <button onClick={addJob}>Add Job</button>
+      <button onClick={removeJob}>Remove Job</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 };
