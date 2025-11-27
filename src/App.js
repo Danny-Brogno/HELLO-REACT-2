dd1b0828d0de0ceae77175670a4f75f7631d8094
danny <danny.brogno@gmail.com>
2025-11-27 03:26:23 +0100

    task completed with addition of React Hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const JobCounter = () => {
+  
+  // let jobCount = 0;
+  // const handleAddJob = () => {
+  //   jobCount++;
+  //   console.log(jobCount); 
+  // }
+  
+  const [first, second] = useState(0);
+  
+  function handleAddJob() {
+    second(first + 1);
+    console.log(first + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Job Counter</h1>
+      <p>Current Jobs: {first}</p>
+      <button onClick={handleAddJob}>Add Job</button>
     </div>
   );
-}
+};
 
-export default App;
+export default JobCounter;
